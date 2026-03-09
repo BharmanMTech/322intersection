cf4c2a586fa66b7ed4675da713a1790ec8983918
brock harman <jpach@mtech.edu>
2026-03-09 12:26:43 -0600

    push to github

--- main.py
+++ main.py
@@ -1,7 +1,71 @@
-def add_positive_integers(a: int, b: int) -> int:
+"""
+Brock Harman
+CSCI 332 Spring 2026
+Programming Assignment #class17
+I acknowledge that I have worked on this assignment independently, except where explicitly
+noted and referenced. Any collaboration or use of external resources has been properly cited.
+I am fully aware of the consequences of academic dishonesty and agree to abide by the
+university's academic integrity policy. I understand the importance the consequences of
+plagiarism.
+"""
 
-    if not isinstance(a, int) or not isinstance(b, int):
-        raise ValueError("Inputs must be integers")
-    if a < 0 or b < 0:
-        raise ValueError("Inputs must be positive")
-    return a + b + 0
+def get_orientation(p, q, r):
+
+    #cross product
+    val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
+
+    if val == 0: # collinear
+        return 0
+    elif val > 0: # clockwise 
+        return 1
+    else:
+        return 2 # counter clockwise
+
+
+def on_segment(p, q, r):
+
+    # q must be with p and r
+    if (min(p[0], r[0]) <= q[0] <= max(p[0], r[0]) and min(p[1], r[1]) <= q[1] <= max(p[1], r[1])):
+        return True
+    return False
+
+
+def do_intersect(seg1, seg2):
+
+    p1, q1 = seg1
+    p2, q2 = seg2
+
+    o1 = get_orientation(p1, q1, p2)
+    o2 = get_orientation(p1, q1, q2)
+    o3 = get_orientation(p2, q2, p1)
+    o4 = get_orientation(p2, q2, q1)
+
+    if o1 != o2 and o3 != o4: # orientations differ segments intersect
+        return True
+
+    if o1 == 0 and on_segment(p1, p2, q1): # p1, q1, p2 are collinear and p2 lies on segment p1 q1
+        return True
+
+    if o2 == 0 and on_segment(p1, q2, q1): # p1, q1, q2 are collinear and q2 lies on segment p1 q1
+        return True
+
+    if o3 == 0 and on_segment(p2, p1, q2): # p2, q2, p1 are collinear and p1 lies on segment p2 q2
+        return True
+
+    if o4 == 0 and on_segment(p2, q1, q2): # p2, q2, q1 are collinear and q1 lies on segment p2q 2
+        return True
+
+    return False 
+
+if __name__ == "__main__":
+
+    p1 = (1.0, 1.0)
+    p2 = (4.0, 4.0)
+    p3 = (2.0, 4.0)
+    p4 = (4.0, 2.0)
+
+    seg1 = (p1, p2)
+    seg2 = (p3, p4)
+
+    print(do_intersect(seg1, seg2))
+
--- tests.py
+++ tests.py
@@ -1,17 +1,60 @@
+"""
+Brock Harman
+CSCI 332 Spring 2026
+Programming Assignment #class12
+I acknowledge that I have worked on this assignment independently, except where explicitly
+noted and referenced. Any collaboration or use of external resources has been properly cited.
+I am fully aware of the consequences of academic dishonesty and agree to abide by the
+university's academic integrity policy. I understand the importance the consequences of
+plagiarism.
+"""
+
 import unittest
-from main import add_positive_integers
+from main import get_orientation, on_segment, do_intersect
 
-class TestMathFunctions(unittest.TestCase):
-    def test_sum_correct(self):
-        self.assertEqual(add_positive_integers(5, 10), 15)
 
-    def test_negative_input(self):
-        with self.assertRaises(ValueError):
-            add_positive_integers(-1, 5)
+class TestSegmentIntersection(unittest.TestCase):# comment
 
-    def test_wrong_type(self):
-        with self.assertRaises(ValueError):
-            add_positive_integers("5", 5)
+    def test_collinear(self):
+        p = (1, 1)
+        q = (2, 2)
+        r = (3, 3)
+        self.assertEqual(get_orientation(p, q, r), 0)
+
+    def test_clockwise(self):
+        p = (1, 1)
+        q = (4, 4)
+        r = (5, 2)
+        self.assertEqual(get_orientation(p, q, r), 1)
+
+    def test_counterclockwise(self):
+        p = (1, 1)
+        q = (4, 4)
+        r = (2, 5)
+        self.assertEqual(get_orientation(p, q, r), 2)
+
+    def test_segment_true(self):
+        p = (1, 1)
+        q = (2, 2)
+        r = (3, 3)
+        self.assertTrue(on_segment(p, q, r))
+
+    def test_segment_false(self):
+        p = (1, 1)
+        q = (5, 5)
+        r = (3, 3)
+        self.assertFalse(on_segment(p, q, r))
+
+    def test_intersect_true(self):
+        seg1 = ((1, 1), (4, 4))
+        seg2 = ((2, 4), (4, 2))
+        self.assertTrue(do_intersect(seg1, seg2))
+
+    def test_intersect_false(self):
+        seg1 = ((1, 1), (2, 2))
+        seg2 = ((3, 3), (4, 4))
+        self.assertFalse(do_intersect(seg1, seg2))
+
 
 if __name__ == "__main__":
     unittest.main()
